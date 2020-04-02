9aa58c2abf9bfe56ed4f1e5923c0c24549c73b26
gogxi <goggxi@gmail.com>
2020-04-02 20:30:00 +0800

    added fragmentAdapter

--- app/src/main/java/com/gogxi/gogxifilms/MainActivity.java
+++ app/src/main/java/com/gogxi/gogxifilms/MainActivity.java
@@ -1,8 +1,14 @@
 package com.gogxi.gogxifilms;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
+
+import com.gogxi.gogxifilms.adapter.fragmentAdapter;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ViewPager2 viewPager = findViewById(R.id.viewPager);
+        viewPager.setAdapter(new fragmentAdapter(this));
+
+        TabLayout tabLayout = findViewById(R.id.tabLayout);
+        TabLayoutMediator tabLayoutMediator = new TabLayoutMediator(
+                tabLayout, viewPager, new TabLayoutMediator.TabConfigurationStrategy() {
+            @Override
+            public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
+                switch (position){
+                    case 0:
+                        tab.setText("Movie");
+                        tab.setIcon(R.drawable.ic_movie);
+                        break;
+                    case 1 :
+                        tab.setText("TV Show");
+                        tab.setIcon(R.drawable.ic_tv_show);
+                        break;
+                }
+            }
+        });
+        tabLayoutMediator.attach();
     }
 }
